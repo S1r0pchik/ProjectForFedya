abd3e2e69c6e1c452196ba58c6a8f10d98fb5801
S1r0pchik <malcevvova09@gmail.com>
2023-06-19 14:35:37 +0300

    создал проект на spring boot

---
+++ src/main/java/com/S1r0p/ProjectForFedya/ProjectForFedyaApplication.java
@@ -0,0 +1,13 @@
+package com.S1r0p.ProjectForFedya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForFedyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForFedyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/S1r0p/ProjectForFedya/ProjectForFedyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S1r0p.ProjectForFedya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForFedyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
