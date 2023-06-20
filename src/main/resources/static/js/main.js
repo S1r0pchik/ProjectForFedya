8f4e47cb168b1b02e878ed0b7c036e6ec586d2af
S1r0pchik <malcevvova09@gmail.com>
2023-06-20 22:13:18 +0300

    Доделал весь дизайн стартовой страницы

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,7 +9,33 @@ function fun() {
     else if (main.bottom + 80 >= footer.top) {
         $("footer").removeClass("fixed-bottom");
     }
+    if (window.innerWidth > 700) {
+        $('.burger').removeClass('active');
+        $('.headerList').removeClass('active');
+        $('main').removeClass('active');
+        $('footer').removeClass('active');
+        $('.line').removeClass('active');
+    }
 }
+
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.burger').toggleClass('active');
+        $('.headerList').toggleClass('active');
+        $('main').toggleClass('active');
+        $('footer').toggleClass('active');
+        $('.line').toggleClass('active');
+    });
+
+    var footer = document.getElementById('footer').getBoundingClientRect();
+    var main = document.getElementById('main').getBoundingClientRect();
+    if (main.bottom + 200 <= $(window).height()) {
+        $("footer").addClass("fixed-bottom");
+    }
+    else if (main.bottom + 80 >= footer.top) {
+        $("footer").removeClass("fixed-bottom");
+    }
+});
 
 window.onresize = fun;
 window.onload = fun;
