7961b780ad0a8bf0503dfaf7608c6b21d70936da
S1r0pchik <malcevvova09@gmail.com>
2023-06-21 18:09:38 +0300

    Доделал стартовую страницу

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,13 @@
 console.log("haha");
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
 function fun() {
     var footer = document.getElementById('footer').getBoundingClientRect();
     var main = document.getElementById('main').getBoundingClientRect();
@@ -16,6 +25,27 @@ function fun() {
         $('footer').removeClass('active');
         $('.line').removeClass('active');
     }
+
+    var p = document.getElementById('text').getBoundingClientRect();
+    var len = p.left / 4.0;
+    $('#toTop').css('width', 2 * len + 'px');
+    $('#toTop').css('height', 2 * len + 'px');
+    $('#toTop').css('left', len + 'px');
+    $('#main').css('margin-bottom', -2 * len - 2 + 'px');
+}
+
+px_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+
+$(window).resize(function(){isZooming();});
+
+function isZooming(){
+    var newPx_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+    if (newPx_ratio != px_ratio) {
+        px_ratio = newPx_ratio;
+        fun();
+    } else {
+        fun();
+    }
 }
 
 $(document).ready(function() {
@@ -25,16 +55,22 @@ $(document).ready(function() {
         $('main').toggleClass('active');
         $('footer').toggleClass('active');
         $('.line').toggleClass('active');
+        $('#toTop').toggleClass('active');
     });
+    fun();
+});
 
-    var footer = document.getElementById('footer').getBoundingClientRect();
-    var main = document.getElementById('main').getBoundingClientRect();
-    if (main.bottom + 200 <= $(window).height()) {
-        $("footer").addClass("fixed-bottom");
-    }
-    else if (main.bottom + 80 >= footer.top) {
-        $("footer").removeClass("fixed-bottom");
-    }
+$(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 400);
+    });
 });
 
 window.onresize = fun;
